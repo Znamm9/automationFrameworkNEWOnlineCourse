52a29e9ed616fdc613597b4fde363f87c19883bc
unknown <olenalevychkina@gmail.com>
2022-04-26 13:27:35 +0300

    checkTextRestaurantLosAngeles

--- src/test/java/pageObjects/SignInPage.java
+++ src/test/java/pageObjects/SignInPage.java
@@ -1,5 +1,6 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -11,5 +12,18 @@ public class SignInPage extends BasePage {
 
     public WebElement getSignUpBtn() {
         return getElementByXpath("//a[contains(text(), 'Sign Up')]");
+    }
+
+    public void closeModal() {
+        driver.findElement(By.xpath("//button[contains(text(),'Got it')]"));
+    }
+    public WebElement getEmailField() {
+        return driver.findElement(By.xpath("//input[@id='email']"));
+    }
+    public WebElement getPassword() {
+        return driver.findElement(By.xpath("//input[@id='password']"));
+    }
+    public WebElement getLoginBtn() {
+        return driver.findElement(By.xpath("//button[@id='signin']"));
     }
 }
