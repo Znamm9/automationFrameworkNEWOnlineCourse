8e05a75744a5e767105850c34555c668d0f9a07a
6ixty6even <1077stargonkiff7@gmail.com>
2022-04-29 12:23:00 +0300

    commitbelongs to6ixty6even

---
+++ src/test/java/tests/CheckTexasRestaurant.java
@@ -0,0 +1,21 @@
+package tests;
+import org.openqa.selenium.devtools.v98.page.model.NavigationEntry;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.HomePage;
+
+public class CheckTexasRestaurant extends TestInit {
+
+    @Test
+    public void TestTexasRestaurant() {
+        HomePage homePage = new HomePage(driver);
+        homePage.navigate();
+        homePage.getSearchInput().sendKeys("Texas City, TX, USA");
+        sleep(2);
+        homePage.getFedBtn().click();
+        sleep(3);
+
+        Assert.assertTrue(homePage.restaurantTexasPizza().isDisplayed());
+    }
+}
+
